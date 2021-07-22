5952a7d5f57794fe19d8c2b07d2f924eb0684ffb
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-07-23 02:16:03 +0700

    Update play.py

--- MusicKen/modules/play.py
+++ MusicKen/modules/play.py
@@ -539,7 +539,7 @@ async def play(_, message: Message):
     elif urls:
         query = toxt
         await lel.edit("🎵 **Sedang Memproses Lagu**")
-        ydl_opts = {"format": "bestaudio/best"}
+        ydl_opts = {"format": "141/bestaudio[ext=m4a]"}
         try:
             results = YoutubeSearch(query, max_results=1).to_dict()
             url = f"https://youtube.com{results[0]['url_suffix']}"
@@ -582,7 +582,7 @@ async def play(_, message: Message):
             query += " " + str(i)
         print(query)
         await lel.edit("🎵 **Sedang Memproses Lagu**")
-        ydl_opts = {"format": "bestaudio/best"}
+        ydl_opts = {"format": "141/bestaudio[ext=m4a]"}
         
         try:
           results = YoutubeSearch(query, max_results=5).to_dict()
@@ -764,7 +764,7 @@ async def ytplay(_, message: Message):
         query += " " + str(i)
     print(query)
     await lel.edit("🎵 **Sedang Memproses Lagu**")
-    ydl_opts = {"format": "bestaudio/best"}
+    ydl_opts = {"format": "141/bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
         url = f"https://youtube.com{results[0]['url_suffix']}"
